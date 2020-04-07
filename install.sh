b757a6d10169245ceffef40c211a1841114d1aa2
Whillikers <aidanswope@gmail.com>
2020-04-07 15:23:23 -0700

    Add i3 config

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 DOTFILES_DIR=${SCRIPT_DIR}/dotfiles
 CONFIG_DIR=${HOME}/.config
 mkdir -p $CONFIG_DIR
+cd submodules/base16-builder-python
 
 echo "== Building base16 templates and colorschemes =="
-cd submodules/base16-builder-python
 pybase16.py update
 pybase16.py build -o "${SCRIPT_DIR}/base16_output" -s $COLORSCHEME \
     -t dunst -t i3 -t kitty -t rofi -t shell -t vim -t xresources
@@ -23,3 +23,7 @@ fc-cache
 echo "== Configuring Kitty =="
 pybase16.py inject -s ${COLORSCHEME} -f ${DOTFILES_DIR}/kitty.conf
 ln -sf ${DOTFILES_DIR}/kitty.conf ${CONFIG_DIR}/kitty/kitty.conf
+
+echo "== Configuring i3 =="
+pybase16.py inject -s ${COLORSCHEME} -f ${DOTFILES_DIR}/i3/config
+ln -sf ${DOTFILES_DIR}/i3 ${CONFIG_DIR}/
