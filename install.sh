9a8057f0ce326d36e3a262043c139fe6eff3a678
Whillikers <aidanswope@gmail.com>
2020-04-07 14:46:17 -0700

    Small cleanup of install script

--- install.sh
+++ install.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(dirname $(realpath $0))
-DOTFILES_DIR=${SCRIPT_DIR}/dotfiles
-
 # Config
 COLORSCHEME="gigavolt"
 
-echo "== Setting up base16 templates and colorschemes =="
+echo "== Setting up =="
+SCRIPT_DIR=$(dirname $(realpath $0))
+DOTFILES_DIR=${SCRIPT_DIR}/dotfiles
+CONFIG_DIR=${HOME}/.config
+mkdir $CONFIG_DIR 2> /dev/null
+
+echo "== Building base16 templates and colorschemes =="
 cd submodules/base16-builder-python
 pybase16.py update
 pybase16.py build -o "${SCRIPT_DIR}/base16_output" -s $COLORSCHEME \
     -t dunst -t i3 -t kitty -t rofi -t shell -t vim -t xresources
 
 echo "== Installing fonts =="
-(mkdir ~/.fonts 2> /dev/null) || true
-ln -sf ${SCRIPT_DIR}/fonts/*.ttf ~/.fonts
+FONT_DIR=${HOME}/.fonts
+mkdir $FONT_DIR 2> /dev/null
+ln -sf ${SCRIPT_DIR}/fonts/*.ttf $FONT_DIR
 fc-cache
 
-echo "== Installing Kitty config =="
+echo "== Configuring Kitty =="
 pybase16.py inject -s ${COLORSCHEME} -f ${DOTFILES_DIR}/kitty.conf
-ln -sf ${DOTFILES_DIR}/kitty.conf ${HOME}/.config/kitty/kitty.conf
+ln -sf ${DOTFILES_DIR}/kitty.conf ${CONFIG_DIR}/kitty/kitty.conf
