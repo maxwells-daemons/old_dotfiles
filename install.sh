93842b5752efaa3a8f11d4992bc6f2de230d4805
Whillikers <aidanswope@gmail.com>
2020-04-07 14:20:56 -0700

    Add Kitty config

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
-DOTFILES_DIR=$(dirname $(realpath $0))
+SCRIPT_DIR=$(dirname $(realpath $0))
+DOTFILES_DIR=${SCRIPT_DIR}/dotfiles
 
 # Config
 COLORSCHEME="gigavolt"
 
-# Build base16 colorscheme and templates
-echo "== base16 setup =="
+echo "== Setting up base16 templates and colorschemes =="
 cd submodules/base16-builder-python
-# pybase16.py update
-pybase16.py build -o "${DOTFILES_DIR}/base16_output" -s $COLORSCHEME \
+pybase16.py update
+pybase16.py build -o "${SCRIPT_DIR}/base16_output" -s $COLORSCHEME \
     -t dunst -t i3 -t kitty -t rofi -t shell -t vim -t xresources
+
+echo "== Installing fonts =="
+(mkdir ~/.fonts 2> /dev/null) || true
+ln -sf ${SCRIPT_DIR}/fonts/*.ttf ~/.fonts
+fc-cache
+
+echo "== Installing Kitty config =="
+pybase16.py inject -s ${COLORSCHEME} -f ${DOTFILES_DIR}/kitty.conf
+ln -sf ${DOTFILES_DIR}/kitty.conf ${HOME}/.config/kitty/kitty.conf
