896aff42a2ab78e21c9e59d0461403cb3e9db25d
Whillikers <aidanswope@gmail.com>
2020-04-08 14:06:01 -0700

    Make XDG_CONFIG_HOME explicit

--- dotfiles/rofi_scripts/layouts.sh
+++ dotfiles/rofi_scripts/layouts.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ -z "$@" ]];
-then ls $HOME/.config/i3/layouts
+then ls ${XDG_CONFIG_HOME}/i3/layouts
 else
-    i3-msg "append_layout ${HOME}/.config/i3/layouts/"$1 > /dev/null
+    i3-msg "append_layout ${XDG_CONFIG_HOME}/i3/layouts/"$1 > /dev/null
 fi
