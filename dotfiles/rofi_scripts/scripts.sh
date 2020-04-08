1cbcf1b06bc4e7afbbee05a0660252e3c3fc65fc
Whillikers <aidanswope@gmail.com>
2020-04-08 05:23:17 -0700

    Fix running scripts with cleaned names

--- dotfiles/rofi_scripts/scripts.sh
+++ dotfiles/rofi_scripts/scripts.sh
@@ -5,6 +5,6 @@ basedir=$HOME/bin/scripts/
 if [[ -z "$@" ]];
 then find $basedir -name "*.sh" | sed 's/.*\/,//;s/\.sh//'
 else
-    eval $basedir$1
+    eval ${basedir},${1}.sh
     i3-msg move window to scratchpad
 fi
